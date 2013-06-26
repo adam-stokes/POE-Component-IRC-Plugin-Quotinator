a73974a8ed9bd27aa16cdc7b4ecddb410cc8819f
Adam Stokes <adam.stokes@ubuntu.com>
2013-06-25 23:29:32 -0400

    integrate mongodb
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/POE/Component/IRC/Plugin/Quotinator.pm
+++ lib/POE/Component/IRC/Plugin/Quotinator.pm
@@ -30,13 +30,14 @@ sub S_public {
     my ($self, $irc) = splice @_, 0, 2;
     my ($nick, $user, $host) = parse_user(${$_[0]});
     my $channel = ${$_[1]}->[0];
-    # Only care about our limited channels as the quote bot can 
+
+    # Only care about our limited channels as the quote bot can
     # get quite offensive. :D
-    return 1 unless $channel eq $self->limit_chan;
-    my $msg     = ${$_[2]};
+    return 1 unless $self->{limit_chan} eq $channel;
+    my $msg = ${$_[2]};
 
     my ($cmd_args) = $msg =~ m/^!quote\s(.*)$/i;
-    my ($help, $add, $del, $search, $get);
+    my ($help, $add, $del, $search, $get, $ext);
     my ($ret, $excess_opt) = GetOptionsFromString(
         $cmd_args,
         'help|usage|?' => \$help,
@@ -44,20 +45,57 @@ sub S_public {
         'del=s'        => \$del,
         'search=s'     => \$search,
         'get=s'        => \$get,
+        'ext'          => \$ext,
     );
 
     if ($help) {
         $irc->yield(
             privmsg => $channel,
-            sprintf("%s: Usage [add|del|search|get] (id|nick) ", $nick)
+            sprintf(
+                "%s: Usage [add|del|search|get] (id|nick) optional [quote]",
+                $nick)
         );
         return PCI_EAT_PLUGIN;
     }
 
     if ($add) {
+        my $quotestr = join(" ", @{$excess_opt});
+        $self->{collection}->insert(
+            {   nick  => $add,
+                quote => $quotestr,
+            }
+        ) unless $self->{collection}->find_one({quote => $quotestr});
         $irc->yield(
             privmsg => $channel,
-            sprintf("%s: adding (%s's) \"%s\"", $nick, $add, join(" ", @{$excess_opt}))
+            sprintf(
+                "%s: adding (%s's) quote \"%s\"",
+                $nick, $add, join(" ", @{$excess_opt})
+            )
+        );
+        return PCI_EAT_PLUGIN;
+    }
+
+    if ($del) {
+        my $quoteid = $del;
+        $self->{collection}->remove({_id => $quoteid,})
+          unless !$self->{collection}->find_one({_id => $quoteid});
+        $irc->yield(
+            privmsg => $channel,
+            sprintf(
+                "%s: removed quote \"%s\"",
+                $nick, join(" ", @{$excess_opt})
+            )
+        );
+        return PCI_EAT_PLUGIN;
+    }
+
+    if ($get) {
+        my $quote = $self->{collection}->find({nick => $get,});
+        $irc->yield(
+            privmsg => $channel,
+            sprintf("%s %s: \"%s\"",
+                $ext ? $quote->{_id} : "", $quote->{nick},
+                $quote->{quote},)
         );
         return PCI_EAT_PLUGIN;
     }
@@ -83,6 +121,21 @@ A mongo database collection must be available and passed into the argument list.
 =head2 B<collection>
 
 MongoDB collection for storing/accessing the quotes.
+
+=head2 B<limit_chan>
+
+Sets which channel the quote plugin will respond too. This is primarily a
+safeguard from previous experiences with offensive quotes ending up in
+the wrong channels :)
+
+=head1 BOT COMMANDS
+
+=head2 B<!quote>
+
+=head3 ARGUMENTS
+
+  -a [nick] (quote)
+  Adds a $quote in the database for $nick.
 
 =head1 LICENSE
 
